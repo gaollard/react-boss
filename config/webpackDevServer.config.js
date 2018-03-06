1c83842e85055dbb02d9de077b6ee5847522b15f
arraybuffer <arraybuffer@foxmail.com>
2018-03-06 22:07:21 +0800

    first commit

--- config/webpackDevServer.config.js
+++ config/webpackDevServer.config.js
@@ -7,7 +7,8 @@ const config = require('./webpack.config.dev');
 const paths = require('./paths');
 
 const protocol = process.env.HTTPS === 'true' ? 'https' : 'http';
-const host = process.env.HOST || '0.0.0.0';
+// const host = process.env.HOST || '0.0.0.0';
+const host = '192.168.1.102'
 
 module.exports = function(proxy, allowedHost) {
   return {
--- src/page/login/login.js
+++ src/page/login/login.js
@@ -1,10 +1,47 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {Button, WingBlank} from 'antd-mobile';
+import {List, InputItem} from 'antd-mobile';
+import './login.css'
+import LogoImg from '@src/asset/img/logo.png'
 
 export default class Login extends Component {
-  render () {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      mobile: '',
+      pwd: ''
+    }
+    this.onChange = this.onChange.bind(this)
+  }
+
+  onChange (key, value) {
+    this.setState({[key]: value})
+  }
+
+  render() {
     return (
       <div className="page-login">
-          
+        <img className="logo" src={ LogoImg } alt="BOSS直聘"/>
+        <div className="login-form">
+          <List>
+            <InputItem
+              clear
+              type="phone"
+              placeholder="手机号码"
+              onChange={ v => this.onChange('mobile', v)}
+              value={this.state.mobile}></InputItem>
+            <InputItem
+              clear
+              type="number"
+              placeholder="登录密码"
+              onChange={(v) => this.onChange('pwd', v)}
+              value={this.state.pwd}></InputItem>
+          </List>
+          <WingBlank className="btn-submit">
+            <Button type="primary">登录</Button>
+          </WingBlank>
+        </div>
       </div>
     )
   }
